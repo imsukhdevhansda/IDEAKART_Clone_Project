bf025662c6231c113eefb74ff7ab18b7ae579d65
Sukhdev Hansda <sukhdevhansda8@gmail.com>
2022-06-14 11:45:20 +0530

    navbar completed

---
+++ component/navbar.js
@@ -0,0 +1,22 @@
+
+let navbar = ()=>{
+
+    return `<nav id="navbar">
+    <div id="part1">
+        <h2> <a href="/">IDEAKART</a> </h2>
+        <div>
+        <input type="text" id="search_books" placeholder="Search">
+        <button id="search_btn">Search</button>
+    </div>
+    </div>
+
+    <div id="part2">
+        <h3> <a href=""></a> About </h3>
+        <h3> <a href=""></a> Contact </h3>
+        <h3> <a href=""></a> Sign In </h3>
+        <h3> <a href=""></a> Sign Up </h3>
+    </div>
+</nav>`
+}
+
+export default navbar
